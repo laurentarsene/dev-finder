fa49820b81c5ed7f438fea6d30dfd7d2787d6457
arsenelaurent <arsene.laurent@student.uclouvain.be>
2022-12-06 07:53:39 +0100

    bio

--- script.js
+++ script.js
@@ -15,102 +15,69 @@ form.addEventListener('submit',function(e){
              width="50%"/>
         `
         if (`${data.name}` == "null") {
-            document.getElementById("name").innerHTML = `
-            Not available
-            `
+            document.getElementById("name").innerHTML = `Not available `
         } else {
-            document.getElementById("name").innerHTML = `
-            ${data.name}
-            `
+            document.getElementById("name").innerHTML = `${data.name}`
         }
 
         if (`${data.login}` == "null") {
-            document.getElementById("login").innerHTML = `
-            Not available
-            `
+            document.getElementById("login").innerHTML = `Not available`
         } else {
-            document.getElementById("login").innerHTML = `
-            ${data.login}
-            `
+            document.getElementById("login").innerHTML = ` ${data.login}`
         }
 
         if (`${data.followers}` == "null") {
-            document.getElementById("followers").innerHTML = `
-            Not available
-            `
+            document.getElementById("followers").innerHTML = `Not available`
         } else {
-            document.getElementById("followers").innerHTML = `
-            ${data.followers}
-            `
+            document.getElementById("followers").innerHTML = `${data.followers}`
         }
         if (`${data.twitter_username}` == "null") {
-            document.getElementById("twitter_username").innerHTML = `
-            Not available
-            `
+            document.getElementById("twitter_username").innerHTML = `Not available`
         } else {
-            document.getElementById("twitter_username").innerHTML = `
-            ${data.twitter_username}
-            `
+            document.getElementById("twitter_username").innerHTML = `${data.twitter_username}`
         }
         if (`${data.location}` == "null") {
-            document.getElementById("location").innerHTML = `
-            Not available
-            `
+            document.getElementById("location").innerHTML = `Not available`
         } else {
-            document.getElementById("location").innerHTML = `
-            ${data.location}
-            `
+            document.getElementById("location").innerHTML = `${data.location}`
         }     
         
         if (`${data.blog}` == "null") {
-            document.getElementById("blog").innerHTML = `
-            Not available
-            `
+            document.getElementById("blog").innerHTML = `Not available`
         } else {
-            document.getElementById("blog").innerHTML = `
-            ${data.blog}
-            `
+            document.getElementById("blog").innerHTML = ` ${data.blog} `
         }        
         
         if (`${data.company}` == "null") {
-            document.getElementById("company").innerHTML = `
-            Not available
-            `
+            document.getElementById("company").innerHTML = ` Not available`
         } else {
-            document.getElementById("company").innerHTML = `
-            ${data.company}
-            `
+            document.getElementById("company").innerHTML = `${data.company}`
         }        
         
         if (`${data.public_repos}` == "null") {
-            document.getElementById("public_repos").innerHTML = `
-            Not available
-            `
+            document.getElementById("public_repos").innerHTML = `Not available `
         } else {
-            document.getElementById("public_repos").innerHTML = `
-            ${data.public_repos}
-            `
+            document.getElementById("public_repos").innerHTML = ` ${data.public_repos}`
         }        
         
         if (`${data.following}` == "null") {
-            document.getElementById("following").innerHTML = `
-            Not available
-            `
+            document.getElementById("following").innerHTML = `Not available`
         } else {
-            document.getElementById("following").innerHTML = `
-            ${data.following}
-            `
+            document.getElementById("following").innerHTML = `${data.following}`
         }        
         
         if (`${data.created_at}` == "null") {
-            document.getElementById("created_at").innerHTML = `
-            Not available
-            `
+            document.getElementById("created_at").innerHTML = `Not available`
         } else {
             var date = `${data.created_at}`.slice(0,10)
             document.getElementById("created_at").innerHTML = date
         }
-        console.log(date)
+
+        if (`${data.bio}` == "null") {
+            document.getElementById("bio").innerHTML = `Not available`
+        } else {
+            document.getElementById("bio").innerHTML = `${data.bio}`
+        }
 
     })
 })
